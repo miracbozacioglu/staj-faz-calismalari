c85968ee6a72e77eb28e2f7efb45853f138216ee
miracbozacioglu <mbozacioglu06@gmail.com>
2026-07-28 18:07:54 +0300

    Gun 4: diziler ve indeks mantigi pratigi

---
+++ Hafta1-Algoritma-CSharp/Gun4-Diziler/Gun4.cs
@@ -0,0 +1,21 @@
+﻿namespace Gun4_Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sehirler = { "İstanbul", "Ankara", "İzmir", "Bursa", "Zonguldak" };
+
+            for (int i = 0; i < sehirler.Length; i++)
+            {
+                Console.WriteLine(sehirler[i]);
+            }
+
+            foreach (string sehir in sehirler)
+            {
+                Console.WriteLine(sehir);
+            }
+
+        }
+    }
+}
