8da1c64ab0a38d0b8c067b0c91a90e26bd404360
miracbozacioglu <mbozacioglu06@gmail.com>
2026-07-28 19:50:39 +0300

    Hafta 2 Gun 1: class ve object, nesne uretme pratigi

---
+++ Hafta-2-Nesne Yönelimli Programlama (OOP)/Gun1-ClassObject/Gun1.cs
@@ -0,0 +1,27 @@
+﻿using static Gun1_ClassObject.Gun1;
+
+namespace Gun1_ClassObject
+{
+    internal class Gun1
+    {
+        public class Ogrenci
+        {
+            public string Ad { get; set; }
+            public double Numara { get; set; }
+            public string Bolum { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+
+            ogrenci.Ad = "Mirac";
+            ogrenci.Numara = 240106109003;
+            ogrenci.Bolum = "Bilgisayar Mühendisliği";
+
+            Console.WriteLine($"Ad: {ogrenci.Ad}");
+            Console.WriteLine($"Numara: {ogrenci.Numara}");
+            Console.WriteLine($"Bölüm: {ogrenci.Bolum}");
+
+        }
+    }
+}
