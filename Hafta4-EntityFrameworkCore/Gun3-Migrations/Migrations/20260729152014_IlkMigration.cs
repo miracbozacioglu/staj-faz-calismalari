8cf4653972da2b85ca212707f51bbd2222a16c3d
miracbozacioglu <mbozacioglu06@gmail.com>
2026-07-29 18:24:18 +0300

    Hafta 4 Gun 3: Code-First migration ile Kullanicilar tablosu olusturma

---
+++ Hafta4-EntityFrameworkCore/Gun3-Migrations/Migrations/20260729152014_IlkMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gun3_Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class IlkMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kullanicilar",
+                columns: table => new
+                {
+                    KullaniciID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    KayitTarihi = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kullanicilar", x => x.KullaniciID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kullanicilar");
+        }
+    }
+}
