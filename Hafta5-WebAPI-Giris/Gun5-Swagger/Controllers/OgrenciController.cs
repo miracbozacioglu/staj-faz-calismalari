70e534914720b6809b46e369e331e1885be74013
miracbozacioglu <mbozacioglu06@gmail.com>
2026-07-30 15:37:22 +0300

    Hafta 5 Gun 4: OgrenciController ve ilk GET endpoint pratigi

---
+++ Hafta5-WebAPI-Giris/Gun5-Swagger/Controllers/OgrenciController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+namespace Gun5_Swagger.Controllers
+{
+    namespace Gun4_Controller.Controllers
+    {
+        [ApiController]                    
+        [Route("api/[controller]")]       
+        public class OgrenciController : ControllerBase
+        {
+            [HttpGet]                      
+            public string Get()
+            {
+                return "Merhaba, bu ilk endpoint'im!";
+            }
+        }
+    }
+}
