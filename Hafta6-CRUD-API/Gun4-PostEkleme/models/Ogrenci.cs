b44a1007c940b49e33f99995c09ed24f24750c26
miracbozacioglu <mbozacioglu06@gmail.com>
2026-07-31 14:20:52 +0300

    Hafta 6 Gun 4: POST ile veri ekleme, JSON donusumu ve sunucu tarafi dogrulama

---
+++ Hafta6-CRUD-API/Gun4-PostEkleme/models/Ogrenci.cs
@@ -0,0 +1,10 @@
+﻿namespace Gun4_PostEkleme.models
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Sinif { get; set; }
+    }
+}
