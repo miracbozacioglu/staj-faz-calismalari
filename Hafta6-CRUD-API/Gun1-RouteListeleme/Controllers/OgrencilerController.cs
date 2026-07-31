dc967175e2e0a74d194e27a241c4a26c9cba2ec2
miracbozacioglu <mbozacioglu06@gmail.com>
2026-07-31 14:02:24 +0300

    Hafta 6 Gun 1: route mantigi ve statik liste donen GET endpoint

---
+++ Hafta6-CRUD-API/Gun1-RouteListeleme/Controllers/OgrencilerController.cs
@@ -0,0 +1,24 @@
+﻿using Gun1_RouteListeleme.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gun1_RouteListeleme.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]      // → api/ogrenciler
+    public class OgrencilerController : ControllerBase
+    {
+        
+        private static List<Ogrenci> _ogrenciler = new List<Ogrenci>
+        {
+            new Ogrenci { Id = 1, Ad = "Mikail", Soyad = "Değer", Sinif = 4 },
+            new Ogrenci { Id = 2, Ad = "Ayşe",   Soyad = "Demir", Sinif = 3 },
+            new Ogrenci { Id = 3, Ad = "Mehmet", Soyad = "Kaya",  Sinif = 2 }
+        };
+
+        [HttpGet]
+        public List<Ogrenci> GetAll()
+        {
+            return _ogrenciler;
+        }
+    }
+}
---
+++ Hafta6-CRUD-API/Gun1-RouteListeleme/models/Ogrenci.cs
@@ -0,0 +1,10 @@
+﻿namespace Gun1_RouteListeleme.models
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Sinif { get; set; }
+    }
+}
